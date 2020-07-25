a8410cfc5a8e173ea23266c8e57335ad5f5e9e2d
Rodrigo <rodrigo.hammes7@gmail.com>
2020-07-25 18:25:26 -0300

    Cria estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  h1. h2, h3, h4, h5, h6 {
+    font-family: Roboto, sans-serif;
+    font-weight: 300;
+  }
+
+  body{
+    -webkit-font-smoothing: antialised;
+  }
+
+  body, input, button{
+    font: 16px Roboto, sans-serif 300;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
