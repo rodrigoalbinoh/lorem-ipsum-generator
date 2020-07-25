a0ebb9a79edafd951fdccfb6ec4bf258bd5c0407
Rodrigo <rodrigo.hammes7@gmail.com>
2020-07-25 18:24:33 -0300

    Cria component de header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return(
+    <Container>
+      <p>Lorem Ipsum Generator</p>
+    </Container>
+  )
+};
+
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.header`
+  background: #000000;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  box-shadow: 0px 10px 5px 1px rgba(0,0,0,0.75);
+  z-index.: 2;
+
+  p{
+    color: #FFF;
+    font-family: Roboto, sans-serif;
+    font-weight: 400;
+    font-size: 22px;
+  }
+`;
